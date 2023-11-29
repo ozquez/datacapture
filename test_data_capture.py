b2ec0dc6eb0c5420ebe8fa37882d49658552bfaf
ozquez <michael.ozquez@gmail.com>
2023-11-28 21:31:24 -0600

    Add test cases for data_capture module

---
+++ test_data_capture.py
@@ -0,0 +1,57 @@
+import unittest
+from data_capture import DataCapture
+
+
+class TestDataCapture(unittest.TestCase):
+    def test_add_valid_numbers(self):
+        capture = DataCapture()
+
+        for number in [3, 9, 3, 4, 6]:
+            capture.add(number)
+
+        self.assertEqual(capture.data[3], 2)  # There are two 3s added
+        self.assertEqual(capture.data[9], 1)  # There is one 9 added
+
+    def test_add_invalid_numbers(self):
+        capture = DataCapture()
+
+        with self.assertRaises(ValueError):
+            capture.add(-1)  # Negative numbers are not allowed
+
+        with self.assertRaises(ValueError):
+            capture.add(capture.SIZE)  # Numbers greater than a fixed
+
+    def test_less_method(self):
+        capture = DataCapture()
+
+        for number in [3, 9, 3, 4, 6]:
+            capture.add(number)
+
+        stats = capture.build_stats()
+        self.assertEqual(stats.less(4), 2)  # Only two numbers (3, 3) are less than 4
+
+    def test_greater_method(self):
+        capture = DataCapture()
+
+        for number in [3, 9, 3, 4, 6]:
+            capture.add(number)
+
+        stats = capture.build_stats()
+        self.assertEqual(
+            stats.greater(4), 2
+        )  # Only two numbers (6, 9) are greater than 4
+
+    def test_between_method(self):
+        capture = DataCapture()
+
+        for number in [3, 9, 3, 4, 6]:
+            capture.add(number)
+
+        stats = capture.build_stats()
+        self.assertEqual(
+            stats.between(3, 6), 4
+        )  # Four numbers (3, 3, 4, 6) are between 3 and 6
+
+
+if __name__ == "__main__":
+    unittest.main()
